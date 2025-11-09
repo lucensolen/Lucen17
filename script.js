225687587aec52558293ba552a31371a89d91f22
lucensolen <lucensolen.studio@gmail.com>
2025-11-09 20:08:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -356,6 +356,7 @@ beam.style.setProperty("--beam-color", mixed);
 beam.style.transition = "background 1s linear, box-shadow 1s linear";
 beam.style.background = mixed;
 beam.style.boxShadow = `0 0 25px 6px ${mixed}`;
+}
 
 // Attach live update listeners safely
 (function wireBeamTone() {
